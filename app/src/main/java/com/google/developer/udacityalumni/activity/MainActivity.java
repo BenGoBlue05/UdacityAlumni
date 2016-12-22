4178d8c074ce63c2932dece84d6720eee4e025a5
Ben Lewis <bplewis5@gmail.com>
2016-12-21 19:48:34 -0600

    add and remove article tablayout on bottom nav selection

--- app/src/main/java/com/google/developer/udacityalumni/activity/MainActivity.java
+++ app/src/main/java/com/google/developer/udacityalumni/activity/MainActivity.java
@@ -1,18 +1,26 @@
 package com.google.developer.udacityalumni.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.facebook.stetho.Stetho;
-import com.google.developer.udacityalumni.fragment.ArticleFragment;
 import com.google.developer.udacityalumni.R;
 import com.google.developer.udacityalumni.adapter.PageAdapter;
+import com.google.developer.udacityalumni.fragment.ArticleFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,12 +48,35 @@ public class MainActivity extends AppCompatActivity {
         Stetho.initializeWithDefaults(this);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
-        if (mViewPager != null){
-            setupViewPager(mViewPager);
-            mTabs.setupWithViewPager(mViewPager);
-        } else{
-            Log.i(LOG_TAG, "mVIEWPAGER IS NULL");
+        setupViewPager(mViewPager);
+        mTabs.setupWithViewPager(mViewPager);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            VectorDrawableCompat indicator
+                    = VectorDrawableCompat.create(getResources(), R.drawable.ic_menu, getTheme());
+            assert indicator != null;
+            indicator.setTint(ResourcesCompat.getColor(getResources(), android.R.color.white, getTheme()));
+            supportActionBar.setHomeAsUpIndicator(indicator);
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        mNavViewBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.articles:
+                        mTabs.setVisibility(View.VISIBLE);
+                        mTabs.setupWithViewPager(mViewPager);
+                        break;
+                    default:
+                        mTabs.removeAllTabs();
+                        mTabs.setVisibility(View.GONE);
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -55,6 +86,28 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(mPageAdapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.careers:
+                Log.i(LOG_TAG, "CAREER TAB SELECTED");
+                mTabs.removeAllTabs();
+                break;
 
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayFragment() {
+
+    }
 }
